0ecdfcfa07c3837053c31153531cdafa3d8a0200
KotariVinay <vinaykotari7@gmail.com>
2024-01-22 22:16:40 +0530

    day15

--- pattern.sh
+++ pattern.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 <<doc
 Name : K.Y.Naga Sai Vinay
-Date : 16-01-2024
+Date : 22-01-2024
 Description : print the pattern
-Sample input : 5
-Sample output : 1 1 1 1 1
-                2 2 2 2
-                3 3 3
-                4 4
-                5
+Sample input :
+Sample output : 1 2 3 4 5 5 4 3 2 1
+                1 2 3 4     5 4 3 2
+                1 2 3         5 4 3
+                1 2             5 4
+                1                 5
 doc
 
-read n
-for i in `seq $n`
+for i in `seq 5`
 do
-    for j in `seq $((n-i+1))`
+    for j in `seq $((5-i+1))`
     do
-	echo -n "$i "
+	echo -n "$j "
+    done
+    for k in `seq $((4*i-4))`
+    do
+	echo -n " "
+    done
+    for l in `seq $((5-i+1))`
+    do
+	echo -n "$((5-l+1)) "
     done
     echo
 done
+
+
